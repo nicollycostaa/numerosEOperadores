eba2adace4605e02ea872746b3496aa0e5cda919
Nicolly Costa <Nicolly65costa@hotmail.com>
2022-07-20 14:11:40 -0300

    commit inicial

---
+++ atividade.js
@@ -0,0 +1,27 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+
+// Crie duas expressões que retornem NaN
+var num1 = +'45s'
+var num2 = 2
+console.log(num1 + num2)
+
+var frase = +"Hello World!"
+console.log(frase)
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 5
+console.log(soma)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var num5 = 5
+console.log(++num5)
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+console.log(peso)
+
+
---
+++ index.js
@@ -0,0 +1,36 @@
+var idade= 28;
+var gols = 1000;
+var pi = 3.14; //ponto para inserir um numero decimal 
+var exp = 2e10; // exponencial na base de 10 = 20000000
+
+console.log(idade);
+console.log(gols);
+console.log(pi);
+console.log(exp);
+
+var soma = 100 + 50;
+var subtracao = 100 - 50;
+var multiplicacao = 100 * 2;
+var divisao = 100 / 2;
+var expoente = 2 ** 4;
+var modulo = 14 % 5;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(expoente);
+console.log(modulo);
+
+var testeNan= 'Isso é 100' / 2
+console.log(isNaN(testeNan))
+
+var x = 5
+console.log(x--)
+console.log(x)
+
+var idade = '28'
+var somaIdade = 5
+console.log(+idade + somaIdade)
+
+var idade = +'28'
